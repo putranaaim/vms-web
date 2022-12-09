35bac2c83d2b4972d90e6b8d7aca1cb6357d985d
sa <as>
2022-12-10 04:05:05 +0800

    Initial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import React from "react";
+import LoginWidget from "./components/loginWidget";
+
+function App() {
+  return (
+    <div className="login-container">
+      <LoginWidget />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/LoginCol-1.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function LoginCol1(props) {
+  return (
+    <div className="row">
+      <img src="" alt="" />
+      <h1>Visitor Management System </h1>
+      <p>
+        Visitor management system helps you improve building safety and employee
+        safety.
+      </p>
+      <img src="" alt="" />
+    </div>
+  );
+}
+
+export default LoginCol1;
